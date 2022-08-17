a5d3d646e1728e8535528cd8bce497f6f86f4818
shay dayan <shay.etl.dayan@gmail.com>
2022-08-17 22:10:01 +0300

    continue working on files

--- playwright_project_with_class/tests_playwright_with_class.py
+++ playwright_project_with_class/tests_playwright_with_class.py
@@ -19,7 +19,7 @@ def get_data_for_test():
 
 
 @pytest.fixture
-def open_main_page(get_data_for_test, request):
+def open_main_page(get_data_for_test:json, request):
     """
     This fixture - will open the page and will close it at the end. with yield.
     :return: None
@@ -37,7 +37,7 @@ def open_main_page(get_data_for_test, request):
         screen_shot_if_faild(driver, request)
         driver.close()
 
-def test_forget_password_button(open_main_page:sync_playwright)->None:
+def test_forget_password_button(open_main_page:MainPage)->None:
     """
     function clicking on forget password in login form.
     :param open_page: sync_playwright: website driver page
@@ -49,7 +49,7 @@ def test_forget_password_button(open_main_page:sync_playwright)->None:
 
 
 
-def screen_shot_if_faild(driver : sync_playwright, request):
+def screen_shot_if_faild(driver : MainPage, request):
     if request.node.rep_call.failed:
         # Make the screen-shot if test failed:
         try:
@@ -62,7 +62,13 @@ def screen_shot_if_faild(driver : sync_playwright, request):
             pass  # just ignore
 
 
-def test_buy_summer(open_main_page, get_data_for_test):
+def test_buy_summer(open_main_page: MainPage, get_data_for_test:json) -> None:
+    """
+    function log in the website and  find the cheapest product under summer search and complete Buying
+    :param open_main_page: sync_playwright: website driver page
+    :param get_data_for_test: json data content
+    :return: None
+    """
     MSG_info("Start Buy Cheap Dress")
     Authentication_page = open_main_page.SignIn()
     MyAccount_page = Authentication_page.login(get_data_for_test["email"], get_data_for_test["password"])
@@ -90,7 +96,13 @@ def test_buy_summer(open_main_page, get_data_for_test):
     assert confirm_order_msg == order_page._order_msg_templet
 
 
-def test_log_in_with_wrong_password(open_main_page, get_data_for_test):
+def test_log_in_with_wrong_password(open_main_page: MainPage, get_data_for_test:json)-> None:
+    """
+    test login with test_log_in_with_wrong_password details to website from get_data_for_test
+    :param open_main_page:  sync_playwright: website driver page
+    :param get_data_for_test: json data content
+    :return: None
+    """
     MSG_info(f"Start log_in_with_wrong_password")
     Authentication_page = open_main_page.SignIn()
     Authentication_page.login(get_data_for_test["email"], "a123asd12")
@@ -99,7 +111,12 @@ def test_log_in_with_wrong_password(open_main_page, get_data_for_test):
     assert error[1] == "Authentication failed."
 
 
-def test_log_in_with_invalid_email(open_main_page):
+def test_log_in_with_invalid_email(open_main_page: MainPage)-> None:
+    """
+    test login with test_login_wrong details to website from get_data_for_test
+    :param open_main_page:  sync_playwright: website driver page
+    :return: None
+    """
     MSG_info(f"Start log_in_with_invalid_email")
     Authentication_page = open_main_page.SignIn()
     Authentication_page.login("shay_dayanail.com", "123456")
@@ -108,16 +125,27 @@ def test_log_in_with_invalid_email(open_main_page):
     assert error[1] == "Invalid email address."
 
 
-def test_log_in_with_short_password(open_main_page):
+def test_log_in_with_short_password(open_main_page: MainPage,get_data_for_test:json)-> None:
+    """
+    test login with test_login_wrong details to website
+    :param open_main_page:  sync_playwright: website driver page
+    :param get_data_for_test: json data content
+    :return: None
+    """
     MSG_info(f"Start log_in_with_short_password")
     Authentication_page = open_main_page.SignIn()
-    Authentication_page.login("shay_dayan@gmail.com", "123")
+    Authentication_page.login(get_data_for_test["email"], "123")
     error = Authentication_page.get_error_msg()
     MSG_info(f"Finish log_in_with_short_password{error}")
     assert error[1] == "Invalid password."
 
 
-def test_log_in_with_no_details(open_main_page):
+def test_log_in_with_no_details(open_main_page: MainPage)-> None:
+    """
+    test login with log_in_with_no_details details to website
+    :param open_main_page:  MainPage: website driver page
+    :return: None
+    """
     MSG_info(f"Start log_in_with_no_details")
     Authentication_page = open_main_page.SignIn()
     Authentication_page.login("", "")
@@ -126,7 +154,14 @@ def test_log_in_with_no_details(open_main_page):
     assert error[1] == "An email address required."
 
 
-def test_log_in_with_right_details(open_main_page, get_data_for_test):
+def test_log_in_with_right_details(open_main_page: MainPage, get_data_for_test:json)-> None:
+    """
+    test login with log_in_with_right_details details to website
+    :param open_main_page:  MainPage: website driver page
+    :param get_data_for_test: json data content
+
+    :return: None
+    """
     MSG_info(f"Start log_in_with_no_details")
     authentication_page = open_main_page.SignIn()
     my_account_page = authentication_page.login(get_data_for_test["email"], get_data_for_test["password"])
